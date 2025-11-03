0af594a510b492f06b1d5a39367e59ca07d11d91
CHbloom <451139136@qq.com>
2025-11-03 14:05:32 +0800

    run.py

--- SWJTU_OOPAO/tutorials/scao_system/myAoSystem/RL4AO/run.py
+++ SWJTU_OOPAO/tutorials/scao_system/myAoSystem/RL4AO/run.py
@@ -1,7 +1,7 @@
 import argparse
 from agent.PPO import PPO
 from agent.SAC import SACContinuous
-from env.AdaptiveOpticsEnv import AOEnv
+from envs.AdaptiveOpticsEnv import AOEnv
 import torch
 import numpy as np
 import utils
@@ -21,6 +21,7 @@ def parse_args():
     parser.add_argument("--exposure_time", type=float, default=1, help="Camera exposure time (seconds)")
     parser.add_argument("--clock_rate", type=int, default=1000, help="Camera clock rate (Hz)")
     parser.add_argument("--lightRatio", type=float, default=0.3, help="wfs lightRatio")
+    parser.add_argument("--paramFile", type=str, default="paramFile1", help="paramFile")
     return parser.parse_args()
 
 if __name__=='__main__':
@@ -28,9 +29,9 @@ if __name__=='__main__':
 
     # 创建环境
     env = AOEnv(args)
+    save_dir = utils.setup_dirs(args)
     # # 根据选择的算法初始化代理
     if args.agent == "PPO":
-        save_dir = utils.setup_dirs(args)
         actor_lr = 1e-4
         critic_lr = 5e-3
         gamma = 0.9
@@ -122,7 +123,7 @@ if __name__=='__main__':
         actor_lr = 3e-4
         critic_lr = 3e-3
         alpha_lr = 3e-4
-        num_episodes = 100
+        num_episodes = args.episodes
         hidden_dim = 128
         gamma = 0.99
         tau = 0.005  # 软更新参数
@@ -138,24 +139,65 @@ if __name__=='__main__':
                             actor_lr, critic_lr, alpha_lr, target_entropy, tau,
                             gamma, device)
 
-        return_list = utils.train_off_policy_agent(env, agent, num_episodes,
-                                                    replay_buffer, minimal_size,
-                                                    batch_size)
+        return_list = []
+        average_sr_list = []  # 一回合平均斯特列尔比
+        average_sr_list_old = []
+        average_wfe_list = []  # 波前误差
 
+        best_mean_return = -np.inf
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        for i in range(10):
+            with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+                for i_episode in range(int(num_episodes / 10)):
+                    state, _ = env.reset(seed=0)
+                    episode_return = 0
+                    episode_sr = []
+                    episode_sr_old = []
+                    episode_wfe = []
+                    done = False
+                    while not done:
+                        action = agent.take_action(state)
+                        next_state, reward, terminated, truncated, _ = env.step(action)
+                        done = terminated or truncated
+                        replay_buffer.add(state, action, reward, next_state, done)
+                        state = next_state
+                        episode_return += reward
+                        episode_sr.append(env.SR[env.current_step-1])
+                        episode_sr_old.append(env.SR_old[env.current_step-1])
+                        episode_wfe.append(env.residual[env.current_step-1])
+                        if replay_buffer.size() > minimal_size:
+                            b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
+                            transition_dict = {'states': b_s, 'actions': b_a, 'next_states': b_ns, 'rewards': b_r,
+                                            'dones': b_d}
+                            agent.update(transition_dict)
+                    return_list.append(episode_return)
+                    average_sr_list.append(np.mean(episode_sr))
+                    average_sr_list_old.append(np.mean(episode_sr_old))
+                    average_wfe_list.append(np.mean(episode_wfe))
+                    # # 保存最佳模型
+                    # current_mean_return = np.mean(return_list[-10:])
+                    # if current_mean_return > best_mean_return:
+                    #     best_mean_return = current_mean_return
+                    #     torch.save({
+                    #         'actor_state_dict': agent.actor.state_dict(),
+                    #         'critic_state_dict': agent.critic.state_dict(),
+                    #         'optimizer_state_dict': agent.actor_optimizer.state_dict(),
+                    #         'best_return': best_mean_return
+                    #     }, f"./data/{save_dir}/train/models/best_model_{timestamp}.pth")
 
-        episodes_list = list(range(len(return_list)))
-        plt.plot(episodes_list, return_list)
-        plt.xlabel('Episodes')
-        plt.ylabel('Returns')
-        plt.title('SAC on {}'.format(env_name))
-        plt.show()
+                    if i_episode % 10 == 0 and len(return_list) >= 10:
+                        pbar.set_postfix({'episode': '%d' % (num_episodes / 10 * i + i_episode + 1),
+                                        'return': f"{episode_return:.1f}",
+                                        'avg_SR': f"{np.mean(episode_sr):.3f}",
+                                        'avg_WFE': f"{np.mean(episode_wfe):.1f}nm"
+                                        })
+                    pbar.update(1)
+        # # 最终保存
+        # torch.save(agent.Actor.state_dict(), f"./data/{save_dir}/train/models/final_actor_{timestamp}.pth")
+        # torch.save(agent.Critic.state_dict(), f"./data/{save_dir}/train/models/final_critic_{timestamp}.pth")
+        utils.save_training_data(return_list, average_sr_list, average_wfe_list, save_dir, average_sr_list_old)
 
-        mv_return = utils.moving_average(return_list, 9)
-        plt.plot(episodes_list, mv_return)
-        plt.xlabel('Episodes')
-        plt.ylabel('Returns')
-        plt.title('SAC on {}'.format(env_name))
-        plt.show()
+        utils.test_agent(agent, env, save_dir)
 
     elif args.agent == "Dreamer-v3":
         pass
